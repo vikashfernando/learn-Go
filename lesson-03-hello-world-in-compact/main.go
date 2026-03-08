a83c984b2c933b48ff182d6c979496422c43c82b
vikashfernando <vikashdeshitha@gmail.com>
2026-03-08 18:59:26 +0530

    added the compact code with multiline comment

--- lesson-03-hello-world-in-compact/main.go
+++ lesson-03-hello-world-in-compact/main.go
@@ -5,3 +5,15 @@ import (
 )
 
 func main() { fmt.Println("this is the same things but in compact mode") }
+
+/*
+
+
+the same code written in compact mode
+
+
+package main;import("fmt");func main(){fmt.Println("this is the same things but in compact mode")};
+
+
+
+*/
