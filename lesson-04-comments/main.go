f2255bf2594131dfcbd7fac6f0788c2e14fa92c1
vikashfernando <vikashdeshitha@gmail.com>
2026-03-08 18:46:35 +0530

    l4 comments done

---
+++ lesson-04-comments/main.go
@@ -0,0 +1,16 @@
+//this is how u add a single line comment in go
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Have a nice day!!!")
+	fmt.Println("im very well")
+
+	/*
+		this is how u add multi line comments in go
+	*/
+}
